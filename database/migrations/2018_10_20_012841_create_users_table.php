29f01ab5f6b52f064059d8d2d9e132b640ee272e
autumnHigh <634579960@qq.com>
2018-11-07 21:16:38 +0800

    修改user表的name唯一字段

--- database/migrations/2018_10_20_012841_create_users_table.php
+++ database/migrations/2018_10_20_012841_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique()->comment('商户名');
+            $table->string('name')->comment('商户名');
             $table->string('email')->unique()->comment('邮件');
             $table->string('password')->comment('密码');
             $table->tinyInteger('status')->comment('商家状态1启用0禁用');
